58a9790b801afdc06bb98229ef1384dd3d6bad2c
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-01 15:20:42 +0300

    8, 9 , 1(2)

--- lab1ex8/Program.cs
+++ lab1ex8/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex 8
+/*
+ Scrieti un program care interschimba valoarea a doua variabile intregi.
+*/
+Console.Write("Introduceti prima valoare: ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Introduceti a doua valoare: ");
+int b = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Inainte: ");
+Console.WriteLine(a + " " + b);
+
+int aux = a;
+a = b;
+b = aux;
+
+Console.WriteLine("Dupa: ");
+Console.WriteLine(a + " " + b);
---
+++ lab2ex1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Ex 1
+/*
+ Scrieti un program care va afisa suma cifrelor unui numar n, n fiind
+citit de la tastatura.
+*/
+
+Console.Write("Introduceti un numar: ");
+int n = int.Parse(Console.ReadLine());
+
+int sumaCifre = 0;
+
+while (n != 0)
+{
+    sumaCifre += (n % 10);
+    n /= 10;
+}
+
+Console.WriteLine("Suma cifrelor este: "+sumaCifre);
