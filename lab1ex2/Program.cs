7865adfc6c5dcfdb2e366b1a829cfe5524756c6e
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-01 11:02:04 +0300

    Ex3

--- lab1ex2/Program.cs
+++ lab1ex2/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Ex2
+/*
+ Scrieti un program care va calcula media aritmetica a trei numere citite de la
+tastatura
+*/
+
+Console.Write("Introduceti primul numar: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Introduceti al 2-lea numar: ");
+int y = int.Parse(Console.ReadLine());
+
+Console.Write("Introduceti al 3-lea numar: ");
+int z = int.Parse(Console.ReadLine());
+
+int medieAritmetica = (x + y + z) / 3;
+Console.WriteLine("Media aritmetica a celor trei numere este: " + medieAritmetica);
+
---
+++ lab1ex3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Ex3
+/*
+ Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la
+tastatura
+*/
+
+Console.Write("Introduceti un numar intreg: ");
+int n = int.Parse(Console.ReadLine());
+
+int ultimaCifra = n % 10;
+Console.WriteLine("Ultima cifra a numarului este:" + c);
