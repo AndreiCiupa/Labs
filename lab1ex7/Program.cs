6fd6e5f6073e032a0a60d0d915c52ffba70b6f29
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-01 15:13:35 +0300

    7

---
+++ lab1ex7/Program.cs
@@ -0,0 +1,13 @@
+﻿// Ex 7
+/*
+  Scrieti un program care va verifica daca un numar citit de la tastatura este par sau
+impar
+ In cazul in care numarul este par, programul va afisa “par” iar in caz contrar, “impar”.
+ Google :even and odd number
+*/
+Console.Write("Introduceti o valoare: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n % 2 == 0)
+    Console.WriteLine("par");
+else Console.WriteLine("impar");
