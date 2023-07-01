5a12974f8d51bf1cf5fade9c3900e378b6ac673b
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-01 15:29:20 +0300

    3(2)

---
+++ lab2ex3/Program.cs
@@ -0,0 +1,35 @@
+﻿// Ex 3
+/*
+  Să se scrie un program care citeşte de la tastatură un şir de n numere
+naturale şi determină media aritmetică a celor pare, n fiind citit de la
+tastatura
+*/
+Console.Write("Introduceti numarul de elemente ale sirului: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] m = new int[n];
+
+int count = 0;
+int mediaAritmetica = 0;
+
+Console.Write("Introduceti elementele sirului: ");
+for (int i = 0; i < n; i++)
+{
+    m[i] = int.Parse(Console.ReadLine());
+}
+
+for (int i = 0; i < n; i++)
+{
+    if (m[i] % 2 == 0)
+    {
+        mediaAritmetica += m[i];
+        count++;
+    }
+}
+
+Console.WriteLine("S = " + mediaAritmetica);
+Console.WriteLine("count = " + count);
+
+mediaAritmetica /= count;
+
+Console.WriteLine("Media aritmetica = " + mediaAritmetica);
