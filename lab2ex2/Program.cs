906953fe98c9d103b8929bf0d46da41c21e309d9
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-01 15:24:05 +0300

    2(2)

---
+++ lab2ex2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Ex 2
+/*
+  Scrieti un program care va afisa factorialul unui numar n, n fiind citit
+de la tastatura.
+*/
+
+Console.Write("Introduceti un numar: ");
+int n = int.Parse(Console.ReadLine());
+
+int x = n;
+int factorial = n;
+
+if (n == 0)
+    Console.WriteLine(1);
+else while (n != 1)
+    {
+        factorial = factorial * (n - 1);
+        n = n - 1;
+    }
+Console.WriteLine(x+"! = "+factorial);
