0ad303e739e73c499080cdd25499c6b55c341b19
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-01 11:07:17 +0300

    5

--- lab1ex4/Program.cs
+++ lab1ex4/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Ex 4
+/*
+  Scrieti un program care va afisa semnul unui numar citit de la tastatura
+• Daca numarul este pozitiv, va afisa “+”
+• Daca numarul este negativ, va afisa “-”
+• Daca numarul este 0, va afisa “0”
+*/
+
+Console.Write("Introduceti un numar: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n == 0)
+    Console.WriteLine(0);
+else if (n > 0)
+    Console.WriteLine("+");
+else Console.WriteLine("-");
---
+++ lab1ex5/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ex 5
+/*
+ Se citesc doua numere de la tastatura, x,y. Scrieti un program care va afisa cele doua valori in ordine crescatoare.
+• Exemplu: citim ,9,0 Afisam : 0 9
+*/
+
+Console.Write("Introduceti prima valoare: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Introduceti a doua valoare: ");
+int y = int.Parse(Console.ReadLine());
+
+if (x == y)
+    Console.WriteLine("Introduceti doua valori distincte.");
+else if (x < y) Console.WriteLine(x + " " + y);
+else Console.WriteLine(y + " " + x);
