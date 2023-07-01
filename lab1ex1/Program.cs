d2a21e55914bb454590aa897979e144008be348b
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-01 10:53:58 +0300

    Add project files.

---
+++ lab1ex1/Program.cs
@@ -0,0 +1,17 @@
+﻿// Ex 1
+/*
+ Scrieti un program care citind de la tastaura cele trei dimensiuni ale unui
+paralelipiped dreptunghic, va afisa volumul lui
+*/
+
+Console.Write("Introduceti lungimea paralelipipedului: ");
+int lungimeParalelipiped = int.Parse(Console.ReadLine());
+
+Console.Write("Introduceti latimea paralelipipedului: ");
+int latimeParalelipiped = int.Parse(Console.ReadLine());
+
+Console.Write("Introduceti inaltimea paralelipipedului: ");
+int inaltimeParalelipiped = int.Parse(Console.ReadLine());
+
+int volumParalelipiped = lungimeParalelipiped * latimeParalelipiped * inaltimeParalelipiped;
+Console.WriteLine("Volumul paralelipipedului este: " + volumParalelipiped);
