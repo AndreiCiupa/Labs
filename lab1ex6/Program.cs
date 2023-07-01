50953f70b7b9423e1cac59bd3a4321a3b2156c74
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-01 11:09:13 +0300

    6

---
+++ lab1ex6/Program.cs
@@ -0,0 +1,52 @@
+﻿// Ex 6
+/*
+  Se citesc trei numere de la tastatura, x,y,z. Scrieti un program care va afisa cele trei valori in ordine
+descrescatoare.
+• Exemplu: citim 3,9,0 Afisam : 9 3 0
+*/
+
+Console.Write("Introduceti prima valoare: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.Write("Introduceti a doua valoare: ");
+int y = int.Parse(Console.ReadLine());
+
+Console.Write("Introduceti a treia valoare: ");
+int z = int.Parse(Console.ReadLine());
+
+if (z > y) // else y < z
+{
+    if (z > x) // else x > z
+    {
+        if (y > x)
+        {
+            Console.WriteLine(z + " " + y + " " + x);
+        }
+        else Console.WriteLine(z + " " + x + " " + y);
+    }
+    else
+    {
+        if (x > y)
+        {
+            Console.WriteLine(x + " " + z + " " + y);
+        }
+        else
+        {
+            Console.WriteLine(x + " " + y + " " + z);
+        }
+    }
+}
+else
+{
+    if (y > x)
+    {
+        if (x > z)
+        {
+            Console.WriteLine(y + " " + x + " " + z);
+        }
+        else
+        {
+            Console.WriteLine(y + " " + z + " " + y);
+        }
+    }
+}
