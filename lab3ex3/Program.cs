ec4cada3060f85bcf57f693dd690dc06e2d70326
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-07 12:14:52 +0300

    3.3

---
+++ lab3ex3/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Lab 3 - Ex 3
+ Scrieti o functie care va determina daca un numar este sau nu patrat perfect.
+Apelati-o si afisati-i rezultatul.
+*/
+
+Console.Write("Introduceti un numar: ");
+int numar = int.Parse(Console.ReadLine());
+
+if (!EstePatratPerfect(numar))
+{
+    Console.WriteLine("Numarul nu este patrat perfect.");
+}
+else
+{ Console.WriteLine("Numarul este patrat perfect."); }
+
+static bool EstePatratPerfect(int x)
+{
+    bool estePatratPerfect = false;
+    for (int d = 2; d * d<= x; d++)
+    {       
+        if(d == x/d)
+        {
+            estePatratPerfect = true;
+        }
+    }
+    return estePatratPerfect;
+}
