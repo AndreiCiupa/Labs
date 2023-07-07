4192ce6ec1e64a80fb05a2023c46ba82007c2c18
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-07 22:00:19 +0300

    4.1

---
+++ lab4ex1/Program.cs
@@ -0,0 +1,42 @@
+﻿/* Lab 4 - Ex 1
+ Scrieti un program care va citi un vector de intregi de dimensiune n de la tastaura. Scrieti o
+functie care va inversa elementele vectorului, apelati-o si afisati-I rezultatul
+*/
+
+Console.Write("Introduceti lungimea vectorului: ");
+int n = int.Parse(Console.ReadLine());
+
+int[] vector = new int[n];
+
+Console.WriteLine("Introduceti elementele vectorului: ");
+for (int i = 0; i < n; i++)
+{
+    vector[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("Vectorul initial: ");
+AfisareVector(vector);
+InversareVector(vector);
+
+Console.WriteLine("Vectorul inversat: ");
+AfisareVector(vector);
+
+static void AfisareVector(int[] v)
+{
+    Console.Write("{ ");
+    for (int i = 0; i < v.Length; i++)
+    {
+        Console.Write(v[i] + " ");
+    }
+    Console.WriteLine("}");
+}
+
+static void InversareVector(int[] v)
+{
+    int j = v.Length - 1;
+    for(int i  = 0; i < v.Length/2; i++)
+    {
+        (v[j], v[i]) = (v[i], v[j]);
+        j--;
+    }
+}
