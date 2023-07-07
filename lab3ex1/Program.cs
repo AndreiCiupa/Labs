b760107d1426e1abc345ecd76c24604ce2e8fcf3
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-07 09:46:20 +0300

    3.1

---
+++ lab3ex1/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Lab3 - Ex 1
+ * Scrieti o functie care va calcula suma cifrelor unui numar.
+*/
+
+Console.Write("Introduceti un numar: ");
+int numar = int.Parse(Console.ReadLine());
+
+int sumaCifrelor = SumaCifrelor(numar);
+
+Console.WriteLine("Suma cifrelor numarului introdus este: " + sumaCifrelor);
+
+static int SumaCifrelor(int x)
+{
+    int suma = 0;
+    while (x > 0)
+    {
+        suma += x%10;
+        x /= 10;
+    }
+    return suma;
+}
