3af574aa8e810a36230346636b336ab619af0586
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-09 18:07:33 +0300

    4.3

--- lab4ex3/Program.cs
+++ lab4ex3/Program.cs
@@ -5,3 +5,80 @@ tastaura. Scrieti o functie care va calcula produsul celor doua matrici, apelati
 rezultatul.
 */
 
+// Se introduc de la tastatura dimensiunile
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+
+Console.Write("m = ");
+int m = int.Parse(Console.ReadLine());
+
+// Se declara cele doua matrici avand cele 2 dimensiuni
+int[,] matriceNM = new int[n, m];
+int[,] matriceMN = new int[m, n];
+
+// Se valorile si se afiseaza cele doua matrici
+for(int i = 0; i < n; i++)
+{
+    for(int j = 0; j < m; j++)
+    {
+        Console.Write("A[{0},{1}] = ",i,j);
+        matriceNM[i,j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write("B[{0},{1}] = ", i, j);
+        matriceMN[i, j] = int.Parse(Console.ReadLine());
+    }
+}
+
+Console.WriteLine();
+
+AfisareMatrice(matriceNM, n, m);
+
+Console.WriteLine();
+
+AfisareMatrice(matriceMN, m, n);
+
+// Produsul lor
+
+Console.WriteLine();
+ProdusMatrice(matriceNM, matriceMN, n, m);
+
+static void AfisareMatrice(int[,] matrice, int n, int m)
+{
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("{ ");
+        for (int j = 0; j < m; j++)
+        {
+            Console.Write(matrice[i, j] + " ");
+        }
+        Console.WriteLine("}");
+    }
+}
+
+static void ProdusMatrice(int[,] A, int[,] B, int n, int m)
+{
+    int dimensiuneProdus = n;
+
+    int[,] produsAB= new int[dimensiuneProdus, dimensiuneProdus];
+
+    for(int i = 0; i < dimensiuneProdus; i++)
+    {
+        for(int j = 0; j < dimensiuneProdus; j++)
+        {
+            produsAB[i, j] = 0;
+            for(int k = 0; k < m; k++)
+            {
+                produsAB[i,j] += A[i, k] * B[k, j];
+            }
+        }
+    }
+    AfisareMatrice(produsAB, dimensiuneProdus, dimensiuneProdus);
+}
