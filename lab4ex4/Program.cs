9f9b0302fdc07d819ecb0c668178f4de69cb5b1e
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-09 18:57:51 +0300

    4.4 Afisare vector recursiv

---
+++ lab4ex4/Program.cs
@@ -0,0 +1,27 @@
+﻿/* Lab 4 Ex 4
+ Scrieti o functie recursiva care va afisa in ordine elementele unui vector de
+intregi.
+*/
+
+Console.Write("Lungimea vectorului: ");
+int lungimeVector = int.Parse(Console.ReadLine());
+
+int[] vector = new int[lungimeVector];
+
+Console.WriteLine("Elementele vectorului: ");
+for (int i = 0; i < lungimeVector; i++)
+{
+    vector[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine();
+
+AfisareVectorRecursiv(vector, lungimeVector - 1);
+
+static void AfisareVectorRecursiv(int[] v, int i)
+{
+    if (i != -1)
+    {
+        AfisareVectorRecursiv(v, i-1);
+        Console.WriteLine(v[i]);
+    }
+}
