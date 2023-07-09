63fe48fe7cce5f4b8f3f962ad6d46062b88a4f9d
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-09 21:11:47 +0300

    4.6 Nth Fibonacci recursiv

---
+++ lab4ex6/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Lab 4 Ex 6
+ Scrieti o functie recursiva care va calcula al n-lea element din sirul lui
+Fibonacci, n fiind citit de la tastatura, apelati-o si afisati-I rezultatul. 
+*/
+
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Al " + n + "-lea numar Fibonacci: " + NthFibonacci(n));
+
+static int NthFibonacci(int n)
+{
+    if (n <= 1)
+    {
+        return n;
+    }
+    else
+    {
+        return NthFibonacci(n - 1) + NthFibonacci(n - 2);
+    }
+}
