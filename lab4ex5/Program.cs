1e7de1b9a4be7fd979a3170664b3092892a047fd
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-09 20:00:57 +0300

    4.5 Suma n numere naturale recursiv

---
+++ lab4ex5/Program.cs
@@ -0,0 +1,16 @@
+﻿/* Lab 4 Ex 5
+  Scrieti o functie recursiva care va calcula suma numerelor de la 1 pana la n,
+apelati-o si afisati-i rezultatul.
+*/
+
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumaN(n));
+
+static int SumaN(int n)
+{
+    if (n != 0)
+    return n + SumaN(n-1); 
+    else return n;
+}
