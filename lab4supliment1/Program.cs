1f2111e4527fb4171d6821a55b1253fa53dea051
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-09 23:00:52 +0300

    4 Ex Suplimentare

---
+++ lab4supliment1/Program.cs
@@ -0,0 +1,18 @@
+﻿/* Lab 4 Supliment 1
+ Se citesc doua numere de la tastatura. Scrieti un program care va calcula
+cel mai mare divizor comun al numerelor
+*/
+
+int a = int.Parse(Console.ReadLine());
+
+int b = int.Parse(Console.ReadLine());
+
+int cmmdc = 1;
+
+while(b != 0)
+{
+    var anteriorB = b;
+    b = a % b;
+    a = anteriorB;
+}
+Console.WriteLine("CMMDC = " +  a);
---
+++ lab4supliment2/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Lab 4 Supliment 2
+  Scrie un program care afiseaza numerele de la 1 la N. Daca numarul este
+multiplu de 3, sa se afiseze Fizz, daca este multiplu de 3 sa se afiseze Buzz,
+daca e multiplu si de 3 si de 5 (ex: 15) sa se afiseze FizzBuzz, in restul
+cazurilor sa se afiseze numarul.
+*/
+
+Console.Write("n = ");
+int n = int.Parse(Console.ReadLine());
+
+int[] intNumere = new int[n];
+
+for(int i = n-1; i>=0; i--)
+{
+    intNumere[i] = i+1;
+}
+
+//Afisare inainte
+for (int i = 0; i < n; i++)
+{
+    Console.Write(intNumere[i] + " ");
+}
+
+Console.WriteLine("\n");
+
+string[] stringNumere = Array.ConvertAll(intNumere, x => x.ToString());
+
+
+for(int i = 0; i < n; i++)
+{
+    if (intNumere[i] % 3 == 0 && intNumere[i] % 5 != 0)
+    {
+        stringNumere[i] = "Fizz";
+    }
+
+    if (intNumere[i] % 3 != 0 && intNumere[i] % 5 == 0)
+    {
+        stringNumere[i] = "Buzz";
+    }
+
+    if (intNumere[i] % 3 == 0 && intNumere[i] % 5 == 0)
+    {
+        stringNumere[i] = "FizzBuzz";
+    }
+}
+
+//Afisare dupa
+for(int i = 0; i < n; i++)
+{
+    Console.Write(stringNumere[i] + " ");
+}
+Console.WriteLine();
