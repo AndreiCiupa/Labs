d8b483c96469e2ac7a1dbbdad13708d16a9f6bb6
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-21 10:10:34 +0300

    5.2

--- lab5ex1/Program.cs
+++ lab5ex1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Lab 5 Ex 1
+ * 
+ Scrieti un program care va afisa pozitia unui substring intr-un string, ambele
+siruri de caractere fiind citite de la tastatura
+*/
+
+Console.Write("Enter string: ");
+string str = Console.ReadLine();
+
+Console.Write("Enter substring: ");
+string subStr = Console.ReadLine();
+
+if (str.Contains(subStr))
+{
+    Console.WriteLine($"Substring starts from index {str.IndexOf(subStr)} of the string.");
+}
+else
+{
+    Console.WriteLine("String does not contain substring.");
+}
---
+++ lab5ex2/Program.cs
@@ -0,0 +1,37 @@
+﻿/* Lab 5 Ex 2
+ * 
+ Scrieti un program care sa verifice daca un sir de caractere citit de la
+tastatura este sau nu palindrom.
+*/
+
+Console.Write("Enter string: ");
+string str = Console.ReadLine();
+
+if (IsPalindrome(str))
+{
+    Console.WriteLine("String is a palindrome.");
+}
+else
+{
+    Console.WriteLine("String is NOT a palindrome.");
+}
+
+static bool IsPalindrome(string s)
+{
+    bool isPalindrome = false;
+    int j = s.Length - 1;
+    for (int i = 0; i < s.Length / 2; i++)
+    {
+        if (s[i] == s[j])
+        {
+            isPalindrome = true;
+        }
+        else 
+        { 
+            isPalindrome = false; 
+            break; 
+        }
+        --j;
+    }
+    return isPalindrome;
+}
