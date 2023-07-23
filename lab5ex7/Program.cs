e70a10e0870c20bb6d4ffc0c0ad78e658727957d
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-23 12:49:08 +0300

    5.7

--- lab5ex7/Program.cs
+++ lab5ex7/Program.cs
@@ -13,24 +13,59 @@ string str2 = Console.ReadLine();
 if(IsAnagram(str1, str2)) { Console.WriteLine("They are anagrams."); }
 else { Console.WriteLine("They are not anagrams."); }
 
-static bool IsAnagram(string str1, string str2)
+static string GetStringNoWhitespace(string str)
 {
-    bool isAnagram = false;
-
-    for(int i = 0; i < str1.Length; i++)
+    for(int i = 0; i < str.Length; i++)
     {
-        for (int j = 0; j < str2.Length; j++)
+        if (/*str[i] == ' ' */char.IsWhiteSpace(str[i]))
         {
-            if (str1[i] == str2[j])
-            {
-                str2 = str2.Substring(0,j) + str2.Substring(j+1);
-            }
+            str = str.Remove(i, 1); // remove from the i-nth (i-nth included!), but just one
         }
     }
 
-    //Scapa de spatii !!!
-    if(string.IsNullOrEmpty(str2)) { isAnagram = true; }
-    else { isAnagram = false; }
+    return str;
+}
+
+static bool IsAnagram(string str1, string str2)
+{
+    bool isAnagram;
+
+    str1 = GetStringNoWhitespace(str1);
+    str1 = str1.ToLower();
+
+    str2 = GetStringNoWhitespace(str2);
+    str2 = str2.ToLower();
+
+    if (str1.Length != str2.Length) { isAnagram = false; }
+    else
+    {
+        for (int i = 0; i < str1.Length; i++)
+        {
+            for (int j = 0; j < str2.Length; j++)
+            {
+                if (str1[i] == str2[j])
+                {
+                    str2 = str2.Substring(0, j) + str2.Substring(j + 1);
+                    break;
+                }
+            }
+            continue;
+        }
+        if (string.IsNullOrEmpty(str2)) { isAnagram = true; }
+        else { isAnagram = false; }
+    }
 
     return isAnagram;
+
+    //int i = 0; 
+    //for (int j = 0; j < str2.Length; j++)
+    //{
+    //    if (str1[i] == str2[j])
+    //    {
+    //        str2 = str2.Substring(0, j) + str2.Substring(j + 1);
+    //        j++;
+    //        str1 = str1.Substring(0, i) + str1.Substring(i + 1);
+    //        i++;
+    //    }
+    //}
 }
