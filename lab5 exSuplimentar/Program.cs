9764c2543d0014db372600d5375ba9610162f188
AndreiCiupa <56363472+AndreiCiupa@users.noreply.github.com>
2023-07-23 16:54:15 +0300

    6.1

---
+++ lab5 exSuplimentar/Program.cs
@@ -0,0 +1,81 @@
+﻿/* Lab 5 Ex Suplimentar
+ * 
+ Catalog
+
+Scrieti un program care va citi de la tastatura un numar n de elevi.
+
+Elevii vor avea nume, prenume precum si un numar de note m specific fiecarui
+elev si citit si el de la tastatura.
+
+Notele fiecarui elev vor fi la randul lor citite de la tastatura.
+
+ Programul va afisa informatiile elevului cu media cea mai mare
+nume, prenume ,medie generala
+
+*/
+
+
+Console.Write("Introduceti numarul de elevi: ");
+int n = int.Parse(Console.ReadLine());
+
+int m;
+
+string[] numeElevi = new string[n];
+string[] prenumeElevi = new string[n];
+int[] numarNoteElevi = new int[n];
+int[,] noteElevi = new int[n,n]; //NU E OK, numarul de note nu e egal cu nrul de elevi...... stuck
+
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Introduceti numele elevului: ");
+    numeElevi[i] = Console.ReadLine();
+
+    Console.Write("Introduceti prenumele elevului: ");
+    prenumeElevi[i] = Console.ReadLine();
+
+    Console.Write("Introduceti numarul de note al elevului: ");
+    m = int.Parse(Console.ReadLine());
+
+    numarNoteElevi[i] = m;
+
+    for(int j = 0; j < m; j++)
+    {
+        Console.Write("Introduceti nota elevului: ");
+        noteElevi[i,j] = int.Parse(Console.ReadLine());
+    }
+}
+
+
+static int[] GetPerformances(int[] numarNoteElevi, int[,] noteElevi)
+{
+    int[] medii = new int[numarNoteElevi.Length];
+
+    for( int i = 0;i < numarNoteElevi.Length; i++)
+    {
+        int medieNote = 0;
+        for( int j = 0; j < numarNoteElevi.Length; j++)
+        {
+            medieNote += noteElevi[i,j];
+        }
+        medieNote /= numarNoteElevi[i];
+        medii[i] = medieNote;
+    }
+    return medii;
+}
+
+static string GetBestStudent(string[] numeElevi, string[] prenumeElevi, int[] medii)
+{
+    string Student = "";
+
+    for( int i = 0;i < medii.Length; i++)
+    {
+        if (medii[i] == medii.Max())
+        {
+            string medie = medii.Max().ToString();
+            Student = numeElevi[i] + ' ' + prenumeElevi[i] + ' ' + medie;
+        }
+    }
+
+    return Student;
+}
---
+++ lab6ex1/Program.cs
@@ -0,0 +1,47 @@
+﻿/* Lab 6 Ex 1
+ * 
+ Un dulap are trei caracteristici : lungime, latime, inaltime. Dulapul va avea o
+metoda care va calcula volumul acestuia.
+
+Folositi o clasa care va modela un dulap.
+
+ Clasa va avea trei campuri private intregi: lungime, latime, inaltime.
+ Cele trei campuri vor fi initializate cu ajutorul constructorului
+ Scrieti o metoda care va calcula volumul dulapului pe baza celor trei campuri.
+
+Instantiati un dulap in functia Main, apelati metoda de calcul a volumului si
+afisati rezultatul
+*/
+
+Console.Write("L = ");
+int L = int.Parse(Console.ReadLine());
+
+Console.Write("l = ");
+int l = int.Parse(Console.ReadLine());
+
+Console.Write("h = ");
+int h = int.Parse(Console.ReadLine());
+
+Dulap dulap = new Dulap(L, l, h);
+
+Console.WriteLine("Volumul dulapului: " + dulap.GetVolume());
+
+class Dulap
+{
+    private int lungime;
+    private int latime;
+    private int inaltime;
+
+    public Dulap(int lungime, int latime, int inaltime)
+    {
+        this.lungime = lungime;
+        this.latime = latime;
+        this.inaltime = inaltime;
+    }
+
+    public int GetVolume()
+    {
+        int volum = this.lungime * this.latime * this.inaltime;
+        return volum;
+    }
+}
